f3c90c7972eac623998ddfd630e8aa57a5fe66fe
Priyanshu Verma <77343606+Priyanshuverma1226@users.noreply.github.com>
2022-12-14 15:47:41 +0530

    unit 9

--- unit 9/basic.py
+++ unit 9/basic.py
@@ -1,17 +1,19 @@
+# define a class
 class student:
+    # Define a variable
     name='Student'
     age=0
-
+# Create a object 1
 s1=student()
 s1.name="Lucky"
 s1.age=20
+# Create a object 2
 s2=student()
 s2.name="Rohan"
 s2.age=18
-
+# Create a object 3 Blank
 s3=student()
-
-
+# Print Data
 print(s1.name)
 print(s2.name)
 print(s3.name)
