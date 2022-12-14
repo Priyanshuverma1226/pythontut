db755e3a837f9ce23e3ff12deb0a770a1ac4b6e9
Priyanshu Verma <77343606+Priyanshuverma1226@users.noreply.github.com>
2022-12-14 14:55:57 +0530

    unit 9

---
+++ unit 9/basic.py
@@ -0,0 +1,17 @@
+class student:
+    name='Student'
+    age=0
+
+s1=student()
+s1.name="Lucky"
+s1.age=20
+s2=student()
+s2.name="Rohan"
+s2.age=18
+
+s3=student()
+
+
+print(s1.name)
+print(s2.name)
+print(s3.name)
