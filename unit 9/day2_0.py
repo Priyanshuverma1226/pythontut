7cd8873453595ae532f33741f1a55c2aa1d1e3c4
Priyanshu Verma <77343606+Priyanshuverma1226@users.noreply.github.com>
2022-12-15 15:22:20 +0530

    unit 9

---
+++ unit 9/day2_0.py
@@ -0,0 +1,21 @@
+
+
+
+class school:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        
+    def show(self):
+        print("Student Name",self.name)
+    def show_age(self):
+        print("Student Age",self.age)
+s1=school("Tanish",15)
+s2=school("Rohan",19)
+s3=school("Manisha",20)
+s4=school("jaspreet",12)
+s3.show()
+s3.show_age()
+s2.show()
+s5=school("Atish",20)
+s5.show()
