72f24afec2da53ff6bb82ff466beade847504c85
Priyanshu Verma <77343606+Priyanshuverma1226@users.noreply.github.com>
2022-12-21 15:09:08 +0530

    unit 10

---
+++ unit 10/pip_topic.py
@@ -0,0 +1,28 @@
+
+
+
+
+import re
+
+
+txt = "The rain in Spain"
+# x = re.findall("ai", txt)
+# x=re.search("\s",txt)
+# x=re.split("\s",txt)
+
+# x=re.sub("\s","        ",txt)
+# email="priyanshu@rjgp.edu.in"
+# x=re.sub("@","[at]",email)
+# x=re.sub("","[dot]",email)
+
+stri="The N* hgsdg sdjfhg Qua"
+
+pattern=re.compile('TP')
+print(pattern)
+result=pattern.findall('TP Tutorialspoint TP')
+print(result)
+
+
+
+
+# print(x)
